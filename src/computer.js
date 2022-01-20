a4a1deea0b96c17459a57b1ff7030f64dbce0b8c
Chris Balogh <chrisconcrete@gmail.com>
2022-01-20 13:28:44 -0700

    Added Computer Class

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,15 @@
 class Computer {
-  
+  constructor(name, operatingSystem, processor, memory, graphics){
+    const {name, operatingSystem, processor, memory, graphics} = this;
+    this.on = false;
+  }
+  power() {
+    if (this.on === false){
+      this.on = true;
+    } else {
+      this.on = false
+    }
+  }
 }
 
 module.exports = Computer
