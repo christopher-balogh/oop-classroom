ee718401e6a8ca05b6e5fd9be2ba73851138fe39
Chris Balogh <chrisconcrete@gmail.com>
2022-01-21 07:22:14 -0700

    Added a Marker Class

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,23 @@
 class Marker {
-  
+  constructor (size, color, remainingInk){
+    this.size = size;
+    this.color = color;
+    this.remainingInk = remainingInk;
+  }
+  write (string){
+    // let n = string.length - (string.split(" ").length-1);
+    let stringArray=[];
+    for (let i=0; i<string.length; i++){
+      if (this.remainingInk || string[i]===" "){
+        stringArray.push(string[i]);
+        if (string[i] !== " "){
+          this.remainingInk--;
+        }
+      }
+    }
+    let printedString = stringArray.join('');
+    return printedString;
+  }
 }
 
 module.exports = Marker
